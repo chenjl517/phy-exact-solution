560bac79763dc5b9216e1d311309c5ed98503c8b
Chenjialin <chenjialin2000@gmail.com>
2021-08-24 09:59:52 +0800

    fix 1DTFIM

--- phy-model/analytical_solution/TFIM_1D.py
+++ phy-model/analytical_solution/TFIM_1D.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+import scipy.integrate as integrate
+from numpy import cosh, cos, log, sqrt
 '''
 
 1D Transverse Field Ising Model(1D TFIM)
@@ -10,8 +11,11 @@ H = \sum_i -J*\sigma_{i}\sigma_{i+1} - h*\sigma_{i}
 
 
 # beta = 1/kT
-# https://en.wikipedia.org/wiki/Ising_model#Ising's_exact_solution
-def free_energy(J, h, beta):
-    free_energy = -1 / beta * np.log(np.exp(beta * J) * np.cosh(beta * h) +
-                                     np.sqrt(np.exp(2 * beta * J) * np.sinh(beta * h) ** 2 + np.exp(-2 * beta * J)))
-    return free_energy
+# Jordan–Wigner transformation
+def lnZ(beta, J, h):
+    lam = J / h
+    w = lambda x: (1 + 2 * lam * cos(x) + lam ** 2) ** 0.5
+    a = lambda x: log(cosh(h * beta * w(x))) / np.pi
+    f2 = integrate.quad(a, 0, np.pi, epsabs=1.0e-13, epsrel=1.0e-13)
+    lnZ = log(2) + f2[0]
+    return lnZ
